11263672ecd9357ee4ab8177e9303c99a480edda
AbdielLuna <abdiel_18luna@outlook.com>
2022-11-03 16:26:48 -0700

    Cambios

--- MediClic_v.0.0.1/main.Designer.cs
+++ MediClic_v.0.0.1/main.Designer.cs
@@ -28,10 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1073, 601);
+            this.Name = "main";
             this.Text = "main";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
