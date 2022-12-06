cbc419af547ae9406ffcc8833a9fa8cfe37dd65a
AbdielLuna <abdiel_18luna@outlook.com>
2022-12-06 16:51:00 -0700

    Listo para el instalar

--- MediClic_v.0.0.1/ConexionDB.cs
+++ MediClic_v.0.0.1/ConexionDB.cs
@@ -11,15 +11,15 @@ namespace MediClic_v._0._0._1
     internal class ConexionDB
     {
         //Conexion en la nube
-        /*static string servidor = "sql8002.site4now.net";
+        static string servidor = "sql8002.site4now.net";
         static string bd = "db_a90a16_mediclicdb";
         static string usuario = "db_a90a16_mediclicdb_admin";
         static string password = "lunal321";
 
         string cadena = "Data Source=" + servidor + ";" + "user id=" + usuario + ";" + "password=" + password + ";" + "Initial Catalog=" + bd + ";" + "Persist Security Info=true";
-        */
+        
         //Conexion de Prueba HostLocal
-        string cadena = "data source = DESKTOP-D9LJ3R4\\SQLEXPRESS; initial catalog = MediClic; Integrated Security=True;";
+        //string cadena = "data source = DESKTOP-D9LJ3R4\\SQLEXPRESS; initial catalog = MediClic; Integrated Security=True;";
 
 
         public SqlConnection Conectarbd = new SqlConnection();
